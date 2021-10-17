da9e210fc852f3d9d6a130fb6bb9367cf4a16ddc
iggeorgiev1979 <iggeorgiev1979@gmail.com>
2021-10-17 19:20:26 +0300

    Added file

--- Advanced/Aditional/Combinatorial/Permutations_without_Repetitions.py
+++ Advanced/Aditional/Combinatorial/Permutations_without_Repetitions.py
@@ -1,13 +1,30 @@
-def combine(symbols: list, index=0):
+# def combine(symbols: list, index=0):
+#     if index == len(symbols):
+#         print(" ".join(symbols))
+#         return
+#     combine(symbols, index + 1)
+#     for i in range(index + 1, len(symbols)):
+#         symbols[index], symbols[i] = symbols[i], symbols[index]
+#         combine(symbols, index + 1)
+#         symbols[index], symbols[i] = symbols[i], symbols[index]
+
+
+def combine(symbols: list, index=0, result=[], used=[]):
+    if not result:
+        result = [None] * len(symbols)
+    if not used:
+        used = [False] * len(symbols)
     if index == len(symbols):
-        print(" ".join(symbols))
+        print(" ".join(result))
         return
-    combine(symbols, index + 1)
-    for i in range(index + 1, len(symbols)):
-        symbols[index], symbols[i] = symbols[i], symbols[index]
-        combine(symbols, index + 1)
-        symbols[index], symbols[i] = symbols[i], symbols[index]
+
+    for i in range(len(symbols)):
+        if not used[i]:
+            result[index] = symbols[i]
+            used[i] = True
+            combine(symbols, index + 1, result, used)
+            used[i] = False
 
 
-text_input = ["A", "B", "C"]
+text_input = ["A", "B", "C", "D"]
 combine(text_input)
