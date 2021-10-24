61e9676bca271faa43fee2c07f7bea29846da905
iggeorgiev1979 <iggeorgiev1979@gmail.com>
2021-10-24 13:59:36 +0300

    Added file

---
+++ Python_OOP/First_Steps_in_OOP/Exercises/project/trainer.py
@@ -0,0 +1,40 @@
+from pokemon import Pokemon
+
+
+class Trainer:
+    def __init__(self, name):
+        self.name = name
+        self.pokemons = []
+
+    def add_pokemon(self, pokemon: Pokemon):
+        if pokemon in self.pokemons:
+            return "This pokemon is already caught"
+        else:
+            self.pokemons.append(pokemon)
+            return f"Caught {pokemon.name} with health {pokemon.health}"
+
+    def release_pokemon(self, pokemon_name: str):
+        for el in self.pokemons:
+            if el.name == pokemon_name:
+                self.pokemons.remove(el)
+                return f"You have released {pokemon_name}"
+        return "Pokemon is not caught"
+
+    def trainer_data(self):
+        pokemon_details = []
+        for el in self.pokemons:
+            pokemon_details.append(f" - {el.pokemon_details()}")
+        details = "\n".join(pokemon_details)
+        return f"Pokemon Trainer {self.name}\nPokemon count {len(self.pokemons)}\n{details}"
+
+
+pokemon = Pokemon("Pikachu", 90)
+print(pokemon.pokemon_details())
+trainer = Trainer("Ash")
+print(trainer.add_pokemon(pokemon))
+second_pokemon = Pokemon("Charizard", 110)
+print(trainer.add_pokemon(second_pokemon))
+print(trainer.add_pokemon(second_pokemon))
+print(trainer.release_pokemon("Pikachu"))
+print(trainer.release_pokemon("Pikachu"))
+print(trainer.trainer_data())
