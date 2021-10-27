46569eedd53b3ec44f0b1f1fcad641a06987a863
iggeorgiev1979 <iggeorgiev1979@gmail.com>
2021-10-28 01:23:20 +0300

    Added file

---
+++ Python_OOP/Inheritance/Stack_of_Strings/stack.py
@@ -0,0 +1,24 @@
+class Stack:
+    def __init__(self):
+        self.data = []
+
+    def push(self, element: str):
+        self.data.append(element)
+
+    def pop(self):
+        return self.data.pop()
+
+    def top(self):
+        return self.data[-1]
+
+    def is_empty(self):
+        return len(self.data) == 0
+
+    def __str__(self):
+        return f"[{', '.join(reversed(self.data))}]"
+
+
+ss = Stack()
+ss.push("apple")
+ss.push("banana")
+print(ss)
