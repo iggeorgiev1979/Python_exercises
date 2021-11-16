88dedf8e3a75931e11903d947933f9ebaabe32a0
iggeorgiev1979 <iggeorgiev1979@gmail.com>
2021-11-16 18:05:05 +0200

    Added file

---
+++ Python_OOP/SOLID/Books.py
@@ -0,0 +1,19 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self.page = 0
+
+    def turn_page(self, page):
+        self.page = page
+
+
+class Library:
+    def __init__(self, books: list):
+        self.books = books
+
+    def find_book(self, title):
+        return [book for book in self.books if book.title == title][0]
+
+
+
