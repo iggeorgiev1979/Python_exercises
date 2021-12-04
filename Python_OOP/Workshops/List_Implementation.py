ba1bab10423f0785677166cce5100fbd10c6170c
iggeorgiev1979 <iggeorgiev1979@gmail.com>
2021-12-04 13:17:27 +0530

    Changed file

--- Python_OOP/Workshops/List_Implementation.py
+++ Python_OOP/Workshops/List_Implementation.py
@@ -1,58 +1,116 @@
+from sys import maxsize
+
 class CustomList:
     def __init__(self):
-        self.store = []
+        self.__store = []
+    
+    def __validate_index(self, index):
+        if index < 0 or index >= len(self.__store):
+            raise IndexError(f"The index must be between 0 and {len(self.__store) - 1}")
     
     def append(self, value):
-        pass
+        self.__store.append(value)
+        return self.__store
 
-    def remove(self, value):
-        pass
+    def remove(self, index):
+        self.__validate_index(index)
+
+        return self.__store.pop(index)
 
     def get(self, index):
-        pass
+        self.__validate_index(index)
+        return self.__store[index]
 
     def extend(self, value):
-        pass
+        if not hasattr(value, '__iter__'):
+            raise Exception("The value is not iterable!")
+        for el in value:
+            self.__store.append(el)
+        return self.__store
 
     def insert(self, index, value):
-        pass
+        self.__store.insert(index, value)
+        return self.__store
 
     def pop(self):
-        pass
+        return self.__store.pop()
 
     def clear(self):
-        self.store = []
+        self.__store = []
 
     def index(self, value):
-        pass
+        for index in range(len(self.__store)):
+            if self.__store[index] == value:
+                return index
+        return f"({value}) is not in the list"
 
     def count(self, value):
-        pass
+        counter = 0
+        for el in self.__store:
+            if el == value:
+                counter += 1
+        return f"({value}) is contained {counter} times in the list."
 
     def reverse(self):
-        pass
+        result = []
+        for index in range(len(self.__store) - 1, - 1, - 1):
+            result.append(str(self.__store[index]))
+        return ", ".join(result)
 
     def copy(self):
-        pass
+        return self.__store.copy()
 
     def size(self):
-        return len(self.store)
+        return len(self.__store)
     
     def add_first(self, value):
-        pass
+        self.__store.insert(0, value)
+        return self.__store
 
     def dictionize(self):
-        pass
+        result = {}
+        for index in range(0, len(self.__store), 2):
+            key = self.__store[index]
+            value = self.__store[index + 1] if index < len(self.__store) - 1 else " "
+            result[key] = value
+        return result
 
     def move(self, amount):
-        pass
+        tmp = self.__store[:amount]
+        self.__store = self.__store[3 - 1:]
+        self.__store.extend(tmp)
+        return self.__store
 
     def sum(self):
-        pass
+        sum_of_the_list = 0
+        for el in self.__store:
+            sum_of_the_list += el if type(el) == int or type(el) == float else len(el)
+        return sum_of_the_list
 
     def overbound(self):
-        pass
+        max_value = -maxsize
+        for el in self.__store:
+            value = el if type(el) == float or type(el) == int else len(el)
+            if value > max_value:
+                max_value = value
+        return max_value
 
     def underbound(self):
-        pass
+        min_value = maxsize
+        for el in self.__store:
+            value = el if type(el) == float or type(el) == int else len(el)
+            if value < min_value:
+                min_value = value
+        return min_value
 
+    def __repr__(self):
+        return f"{', '.join(str(el) for el in self.__store)}"
+
+ll = CustomList()
+ll.append(1)
+ll.append(2)
+ll.append(3)
+ll.append(4)
+ll.append(5.2)
+ll.append("abcsafsetjrsth")
+print(ll.underbound())
