d02a475d5b178d110326b3117397e30436321f58
iggeorgiev1979 <iggeorgiev1979@gmail.com>
2021-12-04 11:51:59 +0530

    Changed file

--- Python_OOP/Workshops/List_Implementation.py
+++ Python_OOP/Workshops/List_Implementation.py
@@ -2,4 +2,57 @@ class CustomList:
     def __init__(self):
         self.store = []
     
+    def append(self, value):
+        pass
+
+    def remove(self, value):
+        pass
+
+    def get(self, index):
+        pass
+
+    def extend(self, value):
+        pass
+
+    def insert(self, index, value):
+        pass
+
+    def pop(self):
+        pass
+
+    def clear(self):
+        self.store = []
+
+    def index(self, value):
+        pass
+
+    def count(self, value):
+        pass
+
+    def reverse(self):
+        pass
+
+    def copy(self):
+        pass
+
+    def size(self):
+        return len(self.store)
     
+    def add_first(self, value):
+        pass
+
+    def dictionize(self):
+        pass
+
+    def move(self, amount):
+        pass
+
+    def sum(self):
+        pass
+
+    def overbound(self):
+        pass
+
+    def underbound(self):
+        pass
+
