614848cfc70f7d8ce0564299486a9e4ce3bc5d2c
iggeorgiev1979 <iggeorgiev1979@gmail.com>
2021-12-09 06:42:33 +0200

    Changed files

--- Python_OOP/Exams/Exam_16_Aug_2020/project/hardware/power_hardware.py
+++
@@ -1,7 +0,0 @@
-from project.hardware.hardware import Hardware
-
-
-class PowerHardware(Hardware):
-    def __init__(self, name: str, capacity: int, memory: int):
-        super().__init__(name, "Power", int(capacity * 0.25), int(memory * 1.75))
-
--- Python_OOP/Exams/Exam_16_Aug_2020/project/software/light_software.py
+++
@@ -1,7 +0,0 @@
-from project.software.software import Software
-
-
-class LightSoftware(Software):
-    def __init__(self, name: str, capacity_consumption, memory_consumption):
-        super().__init__(name, "Light", int(capacity_consumption * 1.5), int(memory_consumption * 0.5))
-
