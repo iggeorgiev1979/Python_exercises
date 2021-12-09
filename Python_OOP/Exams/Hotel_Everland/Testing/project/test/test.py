e3d1b08626f834c28cb3fde38acfa73f179e3a0c
iggeorgiev1979 <iggeorgiev1979@gmail.com>
2021-12-09 20:50:14 +0200

    Changed files

--- Python_OOP/Exams/Hotel_Everland/Testing/project/test/test.py
+++ Python_OOP/Exams/Hotel_Everland/Testing/project/test/test.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.append("..")
+sys.path.append(".")
 from project.student_report_card import StudentReportCard
 from unittest import TestCase, main
 
@@ -23,26 +23,50 @@ class TestCard(TestCase):
         self.assertEqual("Ivan", self.card.student_name)
         self.assertEqual(10, self.card.school_year)
         self.assertEqual({}, self.card.grades_by_subject)
+
+        self.card.school_year = 1
+        self.assertEqual(1, self.card.school_year)
+        self.assertEqual("Ivan", self.card.student_name)
+        self.assertEqual({}, self.card.grades_by_subject)
+
+        self.card.school_year = 12
+        self.assertEqual(12, self.card.school_year)
+        self.assertEqual("Ivan", self.card.student_name)
+        self.assertEqual({}, self.card.grades_by_subject)
+
     
     def test_add_grade(self):
         self.card.add_grade("math", 4)
         self.assertEqual({"math": [4]}, self.card.grades_by_subject)
         self.assertEqual([4], self.card.grades_by_subject["math"])
+        
+    
+    def test_average_grade_by_subject(self):
+        self.card.add_grade("math", 4)
         self.card.add_grade("math", 5)
         self.card.add_grade("bio", 5)
+        self.assertEqual({"math": [4, 5], "bio": [5]}, self.card.grades_by_subject)
+        self.assertEqual([4, 5], self.card.grades_by_subject["math"])
+        self.assertEqual([5], self.card.grades_by_subject["bio"])
         msg = self.card.average_grade_by_subject()
         self.assertEqual("math: 4.50\nbio: 5.00", msg)
 
-    def test_average_grade(self):
+    def test_average_grade_for_all_subjects(self):
         self.card.add_grade("math", 4)
+        self.card.add_grade("math", 5)
+        self.card.add_grade("math", 3)
+        self.card.add_grade("math", 6)
         self.card.add_grade("bio", 5)
+        self.card.add_grade("bio", 2)
+        self.card.add_grade("bio", 4)
+        self.card.add_grade("bio", 6)
         msg = self.card.average_grade_for_all_subjects()
-        self.assertEqual("Average Grade: 4.50", msg)
+        self.assertEqual("Average Grade: 4.38", msg)
 
     def test_repr(self):
         self.card.add_grade("math", 4)
         self.card.add_grade("bio", 5)
-        msg = self.card.__repr__()
+        msg = repr(self.card)
         expected_msg = f"Name: Ivan\n" \
                  f"Year: 10\n" \
                  f"----------\n" \
@@ -50,6 +74,7 @@ class TestCard(TestCase):
                  f"bio: 5.00\n" \
                  f"----------\n" \
                  f"Average Grade: 4.50"
+        self.assertEqual(expected_msg, msg)
 
 
 
